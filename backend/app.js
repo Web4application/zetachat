5011c02dbd6669e8f19e25851b91d8657e542c6f
Web4 <167559384+Web4application@users.noreply.github.com>
2025-03-16 15:38:05 -0400

    Create app.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Zetachat Routes
+const zetachatRoutes = require('./routes/zetachat');
+app.use('/zetachat', zetachatRoutes);
+
+// Start server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
