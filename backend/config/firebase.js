40524970bbec6e5b589ca28c81340f541fd5bde9
Web4 <167559384+Web4application@users.noreply.github.com>
2025-03-18 03:15:31 -0400

    Create firebase.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

---
+++ backend/config/firebase.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey:"AlzaSyC83sBVfbMgcwKilnIEsBzrBXJX5DN4GAM",
+authDomain: "web4-86e33.firebaseapp.com",
+projectId: "web4-86e33",
+storageBucket: "web4-86e33.appspot.com",
+messagingSenderId: "641940543035",
+appId: "1:641940543035:web:b02c6ed23af36dc7dleele",
+measurementId: "G-Q2BPGEX83M"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
