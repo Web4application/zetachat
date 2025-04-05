4acee2174796c3f20a417b4b2c863fd158254c73
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-04 20:14:14 -0400

    Delete models directory
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- models/Todo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TodoSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  completed: { type: Boolean, default: false },
-  deadline: { type: Date, required: false }
-});
-
-module.exports = mongoose.model('Todo', TodoSchema);
