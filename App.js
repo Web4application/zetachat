0857c0cdf3d1067cb88dee78eaf1bc38b76eb083
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-07 22:16:15 -0400

    Create App.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

---
+++ App.js
@@ -0,0 +1,10 @@
+import { Provider } from 'react-redux';
+import store from './store';
+
+function App() {
+    return (
+        <Provider store={store}>
+            <Chat />
+        </Provider>
+    );
+}
