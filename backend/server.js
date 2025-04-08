baf94cce89d26c0d32097340fe5060dc4b2f49be
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-07 22:37:24 -0400

    Create server.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

---
+++ backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.send('Chat app is working!');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+    
+    socket.on('disconnect', () => {
+        console.log('A user disconnected');
+    });
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+});
+
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
+});
