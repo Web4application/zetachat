ab5d03880c3fb11f38d3b56f58ba14c2a13ede87
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-19 06:58:11 -0700

    Update database.js
    Signed-off-by: Web4 <167559384+Web4application@users.noreply.github.com>

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://nulgqjqsqiokhobbhzce.supabase.co'
-const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51bGdxanFzcWlva2hvYmJoemNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDIyOTY4ODEsImV4cCI6MjA1Nzg3Mjg4MX0.2MTbOasHr_oxQZXjcndjmp5sR6gUOmYG7LHVb6bZFS8
+const supabaseUrl = 'https://nyatadsfrhtbfxfnyldv.supabase.co'
+const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
