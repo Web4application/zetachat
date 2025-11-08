4f235e0fc8b6418b042029611b20c997bf00886c
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-08 03:24:14 +0100

    Update streamlit_app.py

--- Pages/streamlit_app.py
+++ Pages/streamlit_app.py
@@ -6,3 +6,10 @@ st.sidebar.selectbox("Group", ["A","B","C"], key="group")
 st.sidebar.slider("Size", 1, 5, key="size")
 
 pg.run()
+def store_value(key):
+    st.session_state[key] = st.session_state["_"+key]
+def load_value(key):
+    st.session_state["_"+key] = st.session_state[key]
+
+load_value("my_key")
+st.number_input("Number of filters", key="_my_key", on_change=store_value, args=["my_key"])
