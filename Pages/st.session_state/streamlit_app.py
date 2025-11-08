0ffce0b6807a21e27a84574e3e3557be5deed8f7
Web4 <167559384+Web4application@users.noreply.github.com>
2025-11-08 03:22:38 +0100

    Create streamlit_app.py

---
+++ Pages/st.session_state/streamlit_app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def store_value():
+    # Copy the value to the permanent key
+    st.session_state["my_key"] = st.session_state["_my_key"]
+
+# Copy the saved value to the temporary key
+st.session_state["_my_key"] = st.session_state["my_key"]
+st.number_input("Number of filters", key="_my_key", on_change=store_value)
