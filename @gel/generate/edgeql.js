55f0119e839d42bffd3fd531a78e021895f83bee
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-06-20 15:14:02 +0100

    Update edgeql.js

--- @gel/generate/edgeql.js
+++ @gel/generate/edgeql.js
@@ -1,1 +1,8 @@
+import { createClient } from "gel"; // or "edgedb"
+import e from "./dbschema/edgeql-js";
+
+const client = createClient();
+
+// The return type of 'result' is automatically inferred as a string
+const result = await e.select("Hello world!").run(client); 
 
